d71bb4538bbc142b3fab14609b51db43e88e0d3f
mahdi <mahdi100th@gmail.com>
2022-03-19 08:21:34 +0430

    run first flask app :)

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     """
     Create and configure the app
     """
-    app = Flask(__name__, intance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev', 
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
