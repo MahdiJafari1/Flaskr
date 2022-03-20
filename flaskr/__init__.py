4462cdebffe60f3648d05baa7f43a3456ddcd182
mahdi <mahdi100th@gmail.com>
2022-03-20 09:11:12 +0430

    part2 access the database completed

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from . import db
 
 
 def create_app(test_config=None):
@@ -8,23 +9,24 @@ def create_app(test_config=None):
     """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev', 
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
     
+    db.init_app(app)
+
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else: 
+        app.config.from_pyfile("config.py", silent=True)
+    else:
         app.config.from_mapping(test_config)
-        
-    
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
-    @app.route('/')
+
+
+    @app.route("/")
     def hello():
-        return 'Hello world!'
-    
+        return "Hello world!"
+
     return app
