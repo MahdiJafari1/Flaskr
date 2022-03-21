34109cffea765c41ebcbd5acbec4d14406c1a936
mahdi <mahdi100th@gmail.com>
2022-03-21 09:16:50 +0430

    part5: blog blueprint completed

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,33 +1,50 @@
 import os
+
 from flask import Flask
-from . import db
-from . import auth
+
 
 def create_app(test_config=None):
-    """
-    Create and configure the app
-    """
+    """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
-    app.register_blueprint(auth.bp)
-    
-    db.init_app(app)
 
     if test_config is None:
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
     else:
-        app.config.from_mapping(test_config)
+        # load the test config if passed in
+        app.config.update(test_config)
 
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-
-    @app.route("/")
+    @app.route("/hello")
     def hello():
-        return "Hello world!"
+        return "Hello, World!"
+
+    # register the database commands
+    from flaskr import db
+
+    db.init_app(app)
+
+    # apply the blueprints to the app
+    from flaskr import auth, blog
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
 
     return app
